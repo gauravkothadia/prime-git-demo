393b0f4980df6a2990432b9809b86ec53ec45e1c
gauravkothadia <gauravkothadia3@gmail.com>
2024-07-20 15:29:34 +0100

    Added name

--- src/main/java/com/studentapp/PrintMyNameMain.java
+++ src/main/java/com/studentapp/PrintMyNameMain.java
@@ -3,5 +3,6 @@ package com.studentapp;
 public class PrintMyNameMain {
     public static void main(String[] args) {
         System.out.println("Hi Hello.");
+        System.out.println("Gaurav");
     }
 }
